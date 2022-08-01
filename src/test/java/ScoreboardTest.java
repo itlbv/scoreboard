8e5e1fc705a68104ca044acdf96c34e641f94bb6
Artem Fokin <artem@favrit.com>
2022-08-01 08:50:38 +0200

    Add empty tests to ScoreboardTest

--- src/test/java/ScoreboardTest.java
+++ src/test/java/ScoreboardTest.java
@@ -1,5 +1,18 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ScoreboardTest {
 
+    @Test
+    void shouldStartNewGameWithZeroScore() {}
+
+    @Test
+    void shouldFinishGameAndRemoveFromScoreboard() {}
+
+    @Test
+    void shouldUpdateScore() {}
+
+    @Test
+    void shouldGetAllGamesSortedByLastUpdated() {}
 }
