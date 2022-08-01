63e7be375d740c130d1f6e188b90282e6057b3be
Artem Fokin <artem@favrit.com>
2022-08-01 09:27:39 +0200

    Add failing shouldStartNewGame test for Scoreboard

--- src/test/java/ScoreboardTest.java
+++ src/test/java/ScoreboardTest.java
@@ -1,11 +1,32 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ScoreboardTest {
 
     @Test
-    void shouldStartNewGameWithZeroScore() {}
+    void shouldStartNewGame() {
+        // given
+        var scoreboard = new Scoreboard();
+
+        // when
+        scoreboard.startNewGame(
+                Team.ARGENTINA,
+                Team.AUSTRALIA
+        );
+
+        // then
+        assertEquals(
+                scoreboard.getGame(
+                        Team.ARGENTINA,
+                        Team.AUSTRALIA
+                ),
+                new Game(
+                        Team.ARGENTINA,
+                        Team.AUSTRALIA
+                )
+        );
+    }
 
     @Test
     void shouldFinishGameAndRemoveFromScoreboard() {}
