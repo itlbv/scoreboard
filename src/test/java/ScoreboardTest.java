a599c043f12bbca7a1d0617a7a557f8a48eb02f4
Artem Fokin <artem@favrit.com>
2022-08-08 09:10:16 +0200

    Add failing tests for sorting logic

--- src/test/java/ScoreboardTest.java
+++ src/test/java/ScoreboardTest.java
@@ -1,6 +1,9 @@
 import exceptions.GameNotFoundException;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -166,6 +169,125 @@ class ScoreboardTest {
         assertThrows(
                 GameNotFoundException.class,
                 () -> scoreboard.updateScore(Team.BRAZIL, 1, 1)
+        );
+    }
+
+    @Test
+    void shouldReturnGamesOrderedByLastStartedWhenEqualTotalScore() {
+        // given
+        var mexicoCanada = new Game(Team.MEXICO, Team.CANADA);
+        mexicoCanada.updateScore(new int[]{2, 2});
+
+        var germanyFrance = new Game(Team.GERMANY, Team.FRANCE);
+        germanyFrance.updateScore(new int[]{4, 0});
+
+        var argentinaAustralia = new Game(Team.ARGENTINA, Team.AUSTRALIA);
+        argentinaAustralia.updateScore(new int[]{1, 3});
+
+        var expectedGamesInOrder = List.of(
+                mexicoCanada,
+                germanyFrance,
+                argentinaAustralia
+        );
+
+        // when
+        var scoreboard = new Scoreboard();
+        scoreboard.startNewGame(Team.ARGENTINA, Team.AUSTRALIA);
+        scoreboard.startNewGame(Team.GERMANY, Team.FRANCE);
+        scoreboard.startNewGame(Team.MEXICO, Team.CANADA);
+
+        scoreboard.updateScore(Team.GERMANY, 4, 0);
+        scoreboard.updateScore(Team.MEXICO, 2, 2);
+        scoreboard.updateScore(Team.ARGENTINA, 1, 3);
+
+        // then
+        assertEquals(
+                expectedGamesInOrder,
+                scoreboard.getGames()
+        );
+    }
+
+    @Test
+    void shouldReturnGamesOrderedByTotalScore() {
+        // given
+        var mexicoCanada = new Game(Team.MEXICO, Team.CANADA);
+        mexicoCanada.updateScore(new int[]{1, 3});
+
+        var argentinaAustralia = new Game(Team.ARGENTINA, Team.AUSTRALIA);
+        argentinaAustralia.updateScore(new int[]{1, 2});
+
+        var uruguayItaly = new Game(Team.URUGUAY, Team.ITALY);
+        uruguayItaly.updateScore(new int[]{1, 1});
+
+        var expectedGamesInOrder = List.of(
+                mexicoCanada,
+                argentinaAustralia,
+                uruguayItaly
+        );
+
+        // when
+        var scoreboard = new Scoreboard();
+        scoreboard.startNewGame(Team.ARGENTINA, Team.AUSTRALIA);
+        scoreboard.startNewGame(Team.URUGUAY, Team.ITALY);
+        scoreboard.startNewGame(Team.MEXICO, Team.CANADA);
+
+        scoreboard.updateScore(Team.ARGENTINA, 1, 2);
+        scoreboard.updateScore(Team.URUGUAY, 1, 1);
+        scoreboard.updateScore(Team.MEXICO, 1, 3);
+
+        // then
+        assertEquals(
+                expectedGamesInOrder,
+                scoreboard.getGames()
+        );
+    }
+
+    @Test
+    void shouldGetAllGamesSortedByTotalScoreAndStartTime() {
+        // given
+        var mexicoCanada = new  Game(Team.MEXICO, Team.CANADA);
+        mexicoCanada.updateScore(new int[]{0, 5});
+
+        var spainBrazil = new  Game(Team.SPAIN, Team.BRAZIL);
+        spainBrazil.updateScore(new int[]{10, 2});
+
+        var germanyFrance = new  Game(Team.GERMANY, Team.FRANCE);
+        germanyFrance.updateScore(new int[]{2, 2});
+
+        var uruguayItaly = new  Game(Team.URUGUAY, Team.ITALY);
+        uruguayItaly.updateScore(new int[]{6, 6});
+
+        var argentinaAustralia = new  Game(Team.ARGENTINA, Team.AUSTRALIA);
+        argentinaAustralia.updateScore(new int[]{3, 1});
+
+        var expectedGamesInOrder = new ArrayList<Game>();
+        expectedGamesInOrder.add(uruguayItaly);
+        expectedGamesInOrder.add(spainBrazil);
+        expectedGamesInOrder.add(mexicoCanada);
+        expectedGamesInOrder.add(argentinaAustralia);
+        expectedGamesInOrder.add(germanyFrance);
+
+        // when
+        var scoreboard = new Scoreboard();
+        scoreboard.startNewGame(Team.MEXICO, Team.CANADA);
+        scoreboard.updateScore(Team.MEXICO, 0, 5);
+
+        scoreboard.startNewGame(Team.SPAIN, Team.BRAZIL);
+        scoreboard.updateScore(Team.SPAIN, 10, 2);
+
+        scoreboard.startNewGame(Team.GERMANY, Team.FRANCE);
+        scoreboard.updateScore(Team.GERMANY, 2, 2);
+
+        scoreboard.startNewGame(Team.URUGUAY, Team.ITALY);
+        scoreboard.updateScore(Team.URUGUAY, 6, 6);
+
+        scoreboard.startNewGame(Team.ARGENTINA, Team.AUSTRALIA);
+        scoreboard.updateScore(Team.ARGENTINA, 3, 1);
+
+        // then
+        assertEquals(
+                expectedGamesInOrder,
+                scoreboard.getGames()
         );
     }
 }
